d3c395e31ed3409138b4dae2a6209c06722b8ffd
Kevin <kevin19960908@gmail.com>
2023-09-25 07:24:02 +0000

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import Cart from '@/views/cart.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [{ path: '/', name: 'home', component: HomeView },{path: '/cart',component: Cart }],
+});
+
+export default router;
---
+++ src/views/cart.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import {movieList} from "./pinia"
+  const movie = movieList().list
+</script>
+
+<template>
+    <main>
+      <router-link to="/" class="nav-link">電影</router-link>
+      <div class="intro">this is cart</div>
+      <div v-for="movie in movie" style="border:1px solid black;display:flex;justify-content:space-between">
+        <div>
+          <div>{{movie.Title}}</div>
+          <div>{{movie.Year}}</div>
+          <div>{{movie.Type}}</div>
+          <div>{{movie.imdbID}}</div>
+          </div>
+          <img  :src="movie.Poster"/>
+      </div>
+    </main>
+</template>
+
+<style scoped></style>
